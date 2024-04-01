c342472f2e69f3adf15ade8db1f5dd518af18706
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-04-01 15:34:35 -0500

    Created movies table in lab5

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 # Add any form classes for Flask-WTF here
 from wtforms import Form
 from wtforms import StringField,TextAreaField, FileField, validators
+from flask_wtf.file import FileAllowed
 
 class MovieForm(Form):
     title = StringField('Title',[validators.InputRequired(),validators.Length(max=50)])
     description = TextAreaField('Description',[validators.InputRequired(),validators.Length(max=0)])
-    poster = FileField('Poster', [validators.InputRequired()])
+    poster = FileField('Poster', [validators.InputRequired(),FileAllowed(['jpg', 'png'], 'Images only!'),])
--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    def __init__():
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String())
+        description = db.Column(db.String())
+        poster = db.Column(db.String())
+        created_at = db.Column(db.DateTime())
